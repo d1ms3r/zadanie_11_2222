86fcf639a52b9434f6f2dbf245ca44bd2c9d3a0c
d1ms3r <d1msd1ms1946@gmail.com>
2024-03-24 18:43:12 +0300

    add func

---
+++ str_func.py
@@ -0,0 +1,8 @@
+def capitalize_first_letters(input_string):
+    """
+    Функция принимает на вход строку и возвращает её со всеми первыми буквами каждого слова в верхнем регистре.
+
+    :param input_string: Входная строка
+    :return: Строка с заглавными первыми буквами каждого слова
+    """
+    return input_string.capitalize()
